1564eb3cba03ba6ed2c1dc799bafdd386d4b546b
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2023-05-20 23:25:53 +0200

    start

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import Aheader from './components/header.vue';
+import Acontenent from './components/contenent.vue';
+import Afooter from './components/footer.vue';
+
+
+export default {
+	components: {Aheader, Acontenent, Afooter }
+};
+</script>
+
+<template>
+  <div class="containet">
+    <Aheader/>
+    <Acontenent/>
+    <Afooter/>
+
+  </div>
+	
+</template>
+
+<style lang="scss">
+@use './assets/styles/general.scss' as *;
+.container{
+  
+}
+
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    data(){
+        return{
+            vociMenu:[
+                'CHARACTCERS',
+                'COMICS',
+                'MOVIE',
+                'GAMES',
+                'COLLECTIBLES',
+                'VIDEO',
+                'FANS',
+                'NEWS',
+                'SHOP']
+        }
+    }
+}
+</script>
+    
+<template>
+    <img src="../assets/img/dc-logo.png" alt="">
+    <ul>
+        <li v-for= "voce in vociMenu">{{ voce }}</li>
+    </ul>
+ 
+</template>
+
+<style lang="sass">
+    
+</style>
